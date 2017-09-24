701c11facefb53cd9718905582fa93f1533a3491
Kaioru <keifuurawrs@gmail.com>
2017-09-24 22:05:10 +0800

    Fix team user table migration

--- database/migrations/2017_09_24_103851_create_team_user_table.php
+++ database/migrations/2017_09_24_103851_create_team_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTeamUsersTable extends Migration
+class CreateTeamUserTable extends Migration
 {
     /**
      * Run the migrations.
