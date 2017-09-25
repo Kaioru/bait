1299c57c21fe608f642c01a108a1d53699335357
Kaioru <keifuurawrs@gmail.com>
2017-09-25 10:01:40 +0800

    Add includes for Star transformers

--- app/Transformers/ArticleStarTransformer.php
+++ app/Transformers/ArticleStarTransformer.php
@@ -4,15 +4,38 @@ namespace App\Transformers;
 
 
 use App\Star;
+use App\User;
 use League\Fractal\TransformerAbstract;
 
 class ArticleStarTransformer extends TransformerAbstract
 {
+    /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected $defaultIncludes = [
+        'user',
+    ];
+
     public function transform(Star $model)
     {
         return [
             'id' => (int)$model->id,
-            'user_id' => (int)$model->user_id,
         ];
+    }
+
+    /**
+     * Include user
+     *
+     * @param Star $model
+     * @return \League\Fractal\Resource\Item|\League\Fractal\Resource\NullResource
+     */
+    public function includeUser(Star $model)
+    {
+        $include = $model->user;
+        return $include
+            ? $this->item($include, new ArticleUserTransformer())
+            : $this->null();
     }
 }
--- app/Transformers/UserStarTransformer.php
+++ app/Transformers/UserStarTransformer.php
@@ -8,11 +8,33 @@ use League\Fractal\TransformerAbstract;
 
 class UserStarTransformer extends TransformerAbstract
 {
+    /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected $defaultIncludes = [
+        'article',
+    ];
+
     public function transform(Star $model)
     {
         return [
             'id' => (int)$model->id,
-            'article_id' => (int)$model->article_id,
         ];
+    }
+
+    /**
+     * Include user
+     *
+     * @param Star $model
+     * @return \League\Fractal\Resource\Item|\League\Fractal\Resource\NullResource
+     */
+    public function includeArticle(Star $model)
+    {
+        $include = $model->article;
+        return $include
+            ? $this->item($include, new PublisherArticleTransformer())
+            : $this->null();
     }
 }
